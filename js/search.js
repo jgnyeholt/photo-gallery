b14bd86f873736b8bbbf39a18375337c326f0dc0
Jonathan <jgnyeholt@gmail.com>
2019-08-08 16:20:34 -0400

    finalized search.js

---
+++ js/search.js
@@ -0,0 +1,28 @@
+$ = document;
+
+const searchBox = $.getElementById("searchBox");
+const searchHeader = $.getElementsByClassName("searchHeader");
+const searchCaption = $.getElementsByClassName("searchCaption");
+const searchGroup = $.getElementsByClassName("searchGroup");
+
+let hidden;
+let visibile;
+
+searchBox.onkeyup = search;
+
+function search(){
+
+	var searchText = searchBox.value.toLowerCase();
+
+	for(var i = 0; i < searchGroup.length; i++){
+		var text = searchHeader[i].innerText.toLowerCase();
+		var caption = searchCaption[i].innerText.toLowerCase();
+
+		if(text.includes(searchText) || caption.includes(searchText)){
+			searchGroup[i].style.display = "inherit";
+		}
+		else if(!(text.includes(searchText) || caption.includes(searchText))){
+			searchGroup[i].style.display = "none";
+		}
+	} //end for loop
+} //end search
